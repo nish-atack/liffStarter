159bb21d4f6f57bcd4671c1b49987a5fcb84b941
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-07 14:34:39 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1657196041-vDWabr0g";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
@@ -138,6 +138,22 @@ function registerButtonHandlers() {
         }
     });
 
+    // scanCode call
+    document.getElementById('scanQrCodeButton').addEventListener('click', function() {
+        if (!liff.isInClient()) {
+            sendAlertIfNotInClient();
+        } else {
+            liff.scanCode().then(result => {
+                // e.g. result = { value: "Hello LIFF app!" }
+                const stringifiedResult = JSON.stringify(result);
+                document.getElementById('scanQrField').textContent = stringifiedResult;
+                toggleQrCodeReader();
+            }).catch(err => {
+                document.getElementById('scanQrField').textContent = "scanCode failed!";
+            });
+        }
+    });
+
     // get access token
     document.getElementById('getAccessToken').addEventListener('click', function() {
         if (!liff.isLoggedIn() && !liff.isInClient()) {
@@ -181,7 +197,7 @@ function registerButtonHandlers() {
             ).catch(function (res) {
                 document.getElementById('shareTargetPickerMessage').textContent = "Failed to launch share target picker.";
             });
-        } 
+        }
     });
 
     // login call, only when external browser is used
@@ -241,5 +257,3 @@ function toggleElement(elementId) {
         elem.style.display = 'block';
     }
 }
-
-
