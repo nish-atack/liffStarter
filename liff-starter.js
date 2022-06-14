46f8e3ee09131a712ec5482e0e05bc5be0c1f01a
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-14 22:49:46 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -184,15 +184,6 @@ function registerButtonHandlers() {
         
 
 
-
-
-var element = "document.getElementById(id)"
- var st =  SpreadsheetApp.openById("1xVJSbo6wOgy1DR0-iryS56f7vAppMdo51q-Do58fjLs").getSheetByName("log");
-  st.getRange(3,2).setValue(element);
-
-    
-            
-            
             
             document.getElementById('statusMessageField').textContent = profile.statusMessage;
             toggleProfileData();
